27eb934bf17df262f6712e2904d6a19e23f93552
Timschaef <tim2003schaefer@gmail.com>
2025-12-18 16:03:25 +0100

    A little bit

--- Assets/+++WORKDATA+++/Scripts/SaveGame/GlobalSaveState.cs
+++ Assets/+++WORKDATA+++/Scripts/SaveGame/GlobalSaveState.cs
@@ -7,4 +7,5 @@ public class GlobalSaveState
     public SaveQuestState[] saveQuestState = new SaveQuestState[0];
     public string[] inventoryItems = new string[0];
     public string[] collectedItemIDs = new string[0];
+    public int saveHP;
 }
